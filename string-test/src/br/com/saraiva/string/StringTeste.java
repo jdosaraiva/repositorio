e7db8c75422d77d68a24b3585bad0cab9f073519
jdosaraiva <jd.saraiva@gmail.com>
2013-12-16 18:42:25 -0200

    continuando o curso de patterns

---
+++ string-test/src/br/com/saraiva/string/StringTeste.java
@@ -0,0 +1,12 @@
+package br.com.saraiva.string;
+
+public class StringTeste {
+
+	public static void main(String[] args) {
+		String a = "01";
+		String b = "1";
+		
+		System.out.println(a.substring(a.length()-1));
+		System.out.println(b.substring(b.length()-1));
+	}
+}
